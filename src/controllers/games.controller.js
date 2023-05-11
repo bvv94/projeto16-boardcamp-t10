7f95c122aa473eb5ef845e06a2db33dfade0e8f8
bvv94 <bvdevasconcelos@gmail.com>
2023-05-11 17:52:37 -0400

    Validações do CRUD games finalizadas

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -14,6 +14,10 @@ export async function createGames(req, res) {
     const { name, image, stockTotal, pricePerDay } = req.body
 
     try {
+        const games = await db.query(`SELECT * FROM games`)
+        const exist = games.rows.find((g) => g.name === name)
+        if (exist) return res.status(409).send("Jogo já existe")
+
         await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
                         VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay])
         res.sendStatus(201)

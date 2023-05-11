08b7ecaa6ee0422e56f3ac967b7c3e189d24034d
bvv94 <bvdevasconcelos@gmail.com>
2023-05-11 18:53:53 -0400

    FEAT Validação de CPF somente para numeros corrigida

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -15,6 +15,10 @@ export async function createRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body
 
     try {
+        const existCustomer = await db.query(`SELECT * FROM customers WHERE id=$1`, [customerId])
+        const existGame = await db.query(`SELECT * FROM games WHERE id=$1`, [gameId])
+        if ((existCustomer.rows.length === 0) || (existGame.rows.length === 0)) return res.sendStatus(400)
+
         await db.query(`INSERT INTO rentals (customerId, gameId, daysRented)
                         VALUES ($1, $2, $3)`, [customerId, gameId, daysRented])
         res.sendStatus(201)

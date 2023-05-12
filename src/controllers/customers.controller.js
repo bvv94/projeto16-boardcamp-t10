6a5c378f6998713b3925f997e70f45dd3d46bd09
bvv94 <bvdevasconcelos@gmail.com>
2023-05-12 18:00:52 -0400

    FEAT formato do birthday corrigido

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -2,7 +2,7 @@ import { db } from "../database/database.connection.js"
 
 export async function getCustomers(req, res) {
     try {
-        const customers = await db.query(`SELECT * FROM customers;`)
+        const customers = await db.query(`SELECT id, name, phone, cpf, to_char(birthday, 'YYYY-MM-DD') AS birthday FROM customers;`)
         res.send(customers.rows)
     }
     catch (err) {
@@ -14,7 +14,7 @@ export async function getCustomersById(req, res) {
     const { id } = req.params
 
     try {
-        const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
+        const customer = await db.query(`SELECT id, name, phone, cpf, to_char(birthday, 'YYYY-MM-DD') AS birthday FROM customers WHERE id=$1;`, [id])
         if (customer.rowCount === 0) return res.sendStatus(404)
 
         res.send(customer.rows[0])
